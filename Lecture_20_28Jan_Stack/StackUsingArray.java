93849f665873e61e4aa0184effabd06a0f04e931
Vaibhav <vkpgpay@gmail.com>
2025-01-28 12:53:29 +0530

    Stack class in java, stack implementation using array

---
+++ Lecture_20_28Jan_Stack/StackUsingArray.java
@@ -0,0 +1,36 @@
+package Lecture_20_28Jan_Stack;
+
+public class StackUsingArray {
+    public static void main(String[] args) {
+       try {
+        MyStack ms = new MyStack();
+        MyStack ms2 = new MyStack(100); // test all the function by yourself
+
+        ms.push(10);
+        ms.push(20);
+        ms.push(30);
+        ms.push(40);
+
+        System.out.println(ms.peek());
+        
+        System.out.println(ms.pop());
+
+        ms.display();
+
+        System.out.println("Size of the stack: " + ms.size());
+
+        System.out.println("Is stack empty? " + ms.isEmpty());
+
+        ms.pop();
+        ms.pop();
+        ms.pop();
+
+        ms.pop();
+
+        System.out.println("Is stack empty? " + ms.isEmpty());
+       } catch (IllegalStateException e) {
+        System.out.println(e.getMessage());
+    }
+
+    }
+}
