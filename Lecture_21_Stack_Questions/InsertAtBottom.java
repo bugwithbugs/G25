971a4b282718a8b59536c457d50f6c6139cd903a
Vaibhav <vkpgpay@gmail.com>
2025-01-30 12:39:27 +0530

    stack questions

---
+++ Lecture_21_Stack_Questions/InsertAtBottom.java
@@ -0,0 +1,49 @@
+package Lecture_21_Stack_Questions;
+
+import java.util.Stack;
+
+public class InsertAtBottom {
+
+    static void bruteForceUsingAnotherStack(Stack<Integer> st, int item){
+        Stack<Integer> temp = new Stack<>();
+        while (!st.isEmpty()) {
+            temp.push(st.pop());
+        }
+
+        st.push(item);
+
+        while (!temp.isEmpty()) {
+            st.push(temp.pop());
+        }
+    }
+
+    static void optimizedApproachUsingRecursion (Stack<Integer> st, int item){
+
+        if(st.isEmpty()){
+            st.push(item);  
+            return;  
+        }
+
+        int top = st.pop();
+        optimizedApproachUsingRecursion(st, item);
+        st.push(top);
+    }
+
+    public static void main(String[] args) {
+
+        InsertAtBottom obj = new InsertAtBottom();
+        Stack<Integer> st = new Stack<>();
+
+        int n = 5;
+        int item = 60; // item to insert
+
+        for(int i = 1; i <= n; i++) {
+            st.push(10 * i);
+        }
+
+        //obj.bruteForceUsingAnotherStack(st, item);
+        obj.optimizedApproachUsingRecursion(st, item);
+        System.out.println(st);
+
+    }
+}
