8990f8cc12757f1b8e5e2271431bb811bcaa6dd6
Vaibhav <vkpgpay@gmail.com>
2025-04-07 14:34:22 +0530

    lecture 44

---
+++ Lecture44_Apr07_ST02_PaperDiscussion_Test/Main.java
@@ -0,0 +1,7 @@
+package Lecture44_Apr07_ST02_PaperDiscussion_Test;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("ST02 Question paper discussion and oral test");
+    }
+}
